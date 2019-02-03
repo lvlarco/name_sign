e1a580dac67d673dfc8476c3da4dfa35431e0528
unknown <marco.campos001@gmail.com>
2019-02-03 18:04:41 -0500

    chanign to rpi gpio

--- src/lcd_adafruit.py
+++ src/lcd_adafruit.py
@@ -1,13 +1,28 @@
+import RPi.GPIO as GPIO
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
 
-lcd_rs = digitalio.DigitalInOut(board.D26)
-lcd_en = digitalio.DigitalInOut(board.D19)
-lcd_d7 = digitalio.DigitalInOut(board.D27)
-lcd_d6 = digitalio.DigitalInOut(board.D22)
-lcd_d5 = digitalio.DigitalInOut(board.D24)
-lcd_d4 = digitalio.DigitalInOut(board.D25)
+# lcd_rs = digitalio.DigitalInOut(board.D26)
+# lcd_en = digitalio.DigitalInOut(board.D19)
+# lcd_d7 = digitalio.DigitalInOut(board.D27)
+# lcd_d6 = digitalio.DigitalInOut(board.D22)
+# lcd_d5 = digitalio.DigitalInOut(board.D24)
+# lcd_d4 = digitalio.DigitalInOut(board.D25)
+
+lcd_rs = 26
+lcd_en = 19
+lcd_d7 = 27
+lcd_d6 = 22
+lcd_d5 = 24
+lcd_d4 = 25
+
+GPIO.setup(lcd_rs, GPIO.OUT)
+GPIO.setup(lcd_en, GPIO.OUT)
+GPIO.setup(lcd_d7, GPIO.OUT)
+GPIO.setup(lcd_d6, GPIO.OUT)
+GPIO.setup(lcd_d5, GPIO.OUT)
+GPIO.setup(lcd_d4, GPIO.OUT)
 
 lcd_columns = 16
 lcd_rows = 2
