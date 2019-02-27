7760a0ec64e1455bc700a5046a0544e174bf0159
unknown <marco.campos001@gmail.com>
2019-02-26 21:18:25 -0500

    lcd working

--- src/lcd_maker.py
+++ src/lcd_maker.py
@@ -4,7 +4,8 @@ from RPLCD.gpio import CharLCD
 GPIO.setwarnings(False)
 
 # Configure the LCD
-lcd = CharLCD(pin_rs = 19, pin_rw = None, pin_e = 16, pins_data = [21,18,23,24], numbering_mode = GPIO.BOARD, cols=16, rows=2, dotsize=8)
+lcd = CharLCD(pin_rs = 19, pin_rw = None, pin_e = 16, pins_data = [21,18,23,24],
+              numbering_mode = GPIO.BOARD, cols=16, rows=2, dotsize=8)
 #lcd = CharLCD(pin_rs = 10, pin_rw = None, pin_e = 23, pins_data = [9,24,11,8], numbering_mode = GPIO.BCM)
 
 number = 0
@@ -13,9 +14,7 @@ number = 0
 while(True):
 # Increment the number and then print it to the LCD number = number + 1
     lcd.clear()
-    lcd.write_string("hi")
-    time.sleep(1)
-    time.sleep(1)
+    lcd.write_string("Marco's office, \nAvailable? ")
 
 lcd.close()
 GPIO.cleanup()
