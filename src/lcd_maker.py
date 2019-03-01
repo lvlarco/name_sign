28bcdc559de92a032750396d633ae896ea3a5af7
Marco Campos <marco.campos001@gmail.com>
2019-02-28 20:28:41 -0500

    adding more lines

--- src/lcd_maker.py
+++ src/lcd_maker.py
@@ -10,9 +10,13 @@ lcd = CharLCD(pin_rs = 19, pin_rw = None, pin_e = 16, pins_data = [21,18,23,24],
 
 while(True):
     lcd.clear()
-    lcd.write_string("Marco Campos' \nOffice")
+    lcd.write_string("Marco Campos' \r\nOffice")
     time.sleep(1)
+    lcd.clear()
     lcd.write_string("Skype Status: ")
+    time.sleep(1)
+    lcd.clear()
+    lcd.write_string("Today is a cold day")
     time.sleep(1)
 
 lcd.close()
