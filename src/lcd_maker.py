164e90a3c2acd3e92e3321468287424932162489
unknown <marco.campos001@gmail.com>
2019-02-28 20:16:13 -0500

    adding slides

--- src/lcd_maker.py
+++ src/lcd_maker.py
@@ -8,16 +8,12 @@ lcd = CharLCD(pin_rs = 19, pin_rw = None, pin_e = 16, pins_data = [21,18,23,24],
               numbering_mode = GPIO.BOARD, cols=16, rows=2, dotsize=8)
 #lcd = CharLCD(pin_rs = 10, pin_rw = None, pin_e = 23, pins_data = [9,24,11,8], numbering_mode = GPIO.BCM)
 
-number = 0
-
-# Main loop
 while(True):
-# Increment the number and then print it to the LCD number = number + 1
     lcd.clear()
-    lcd.write_string("Marco Campos\r\nAvailable? ")
+    lcd.write_string("Marco Campos' \nOffice")
     time.sleep(1)
-#lcd.cursor_pos = (2,1)
-#    lcd.write_string("Available? ")
+    lcd.write_string("Skype Status: ")
+    time.sleep(1)
 
 lcd.close()
 GPIO.cleanup()
